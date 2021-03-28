d8904b0a054ff819aac243e2d97279eeede7a051
stefan <stefansgit@gmail.com>
2021-03-28 14:56:20 +0200

    buttons functionalities

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -3,31 +3,49 @@ var textArea = document.getElementById("textArea");
 var str = "";
 var capsValue = false;
 var value = "";
+var shift = false;
+var resVal = "";
 
 // if(document.getElementById('caps-lock').clicked == true) {
 //         alert("clicked");
 //     };
 function btnClick(id){
     value = document.getElementById(id).innerHTML;
-   
-    textArea.innerHTML += value; 
-    str = textArea.innerHTML;
-    
-    
+    if(shift === true)
+    {
+        capsValue = false;
+        resVal = value.toUpperCase();
+        //resVal.toUpperCase();
+        //alert(resVal);
+        textArea.innerHTML += resVal;
+        shift = false;
+        
+    } else if(capsValue === true){
+        textArea.innerHTML += value.toUpperCase();
+    }
+    else{
+    // if(capsValue === true){
+    //     
+    // } else {
+        textArea.innerHTML += value; 
+        str = textArea.innerHTML;
+    }
 }
-// function capsLock(value) {
-//     capsValue = true;
-//     //textArea.innerHTML -= capsValue;
-//     textArea.innerHTML += value.toUpperCase();
-// }
-
+function capsLock() {
+    capsValue = !capsValue;
+}
+function shiftClick() {
+    shift = true;
+}
+function enterBtn() {
+    textArea.innerHTML += '\r\n';
+}
 
 function btnOk() {
     alert(textArea.innerHTML);
 }
 function backSpace(){
     var newStr = "";
-    //var textArea = document.getElementById("textArea").innerHTML;
     for(var i = 0; i < str.length-1; i++){
         newStr += str[i];
     }
